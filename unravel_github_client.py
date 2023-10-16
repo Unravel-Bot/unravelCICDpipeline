30bad928c397d89076bdd0e6a7cd40ac954d06a3
Unravel code Reviewer <108653287+Unravel-Assistant@users.noreply.github.com>
2023-10-16 17:12:24 +0530

    Update unravel_github_client.py

--- unravel_github_client.py
+++ unravel_github_client.py
@@ -6,6 +6,7 @@ import requests
 import getopt
 import urllib3
 import os
+import time
 import html
 from jira import JIRA
 from bs4 import BeautifulSoup
@@ -53,7 +54,7 @@ email = os.getenv("JIRA_EMAIL")
 project_key = os.getenv("JIRA_PROJECT_KEY")
 api_token = os.getenv("JIRA_API_TOKEN")
 
-
+lr_url = unravel_url+":4043"
 
 file_code_map = {}
 file_code_map['df.toPandas()'] = 'toPandas() moves all the data to driver to convert the spark df to a pandas dataframe.\n\n Instead use this statement df.withColumn("<newColumn>", lit("<constant_value>"))'
@@ -247,7 +248,7 @@ def fetch_app_summary(unravel_url, unravel_token, clusterUId, appId):
     )
 
     estimated_cost = summary_dict["cents"] + summary_dict["dbuCost"]
-    
+
     app_summary_map_for_git_comment["Estimated cost"] = "$ {}".format(
         round(estimated_cost, 3)
     )
@@ -262,6 +263,8 @@ def fetch_app_summary(unravel_url, unravel_token, clusterUId, appId):
         "driver_node_type_id"
     ]
     app_summary_map_for_git_comment["Tags"] = runinfo["default_tags"]
+    # tags = json.loads(runinfo["default_tags"])
+    # job_name = tags['']
     app_summary_map_for_jira_comments["Tags"] = runinfo["default_tags"]
     if "custom_tags" in runinfo.keys():
         app_summary_map_for_git_comment["Tags"] = {
@@ -462,7 +465,7 @@ def create_custom_code_block_and_add_pr_comment(code_block):
     comment += "```\n\n"
     return comment
 
-def perform_code_review():
+def perform_code_review(get_file_name_flag=False):
     # Get the changed file paths from the pull request event payload
     headers = {
         'Authorization': f'Bearer {access_token}',
@@ -472,6 +475,8 @@ def perform_code_review():
     response = requests.get(url, headers=headers)
     files = response.json()
     changed_files = [file['filename'] for file in files]
+    if get_file_name_flag == True:
+        return changed_files
     print(changed_files)
 
     file_contents = {}
@@ -526,12 +531,90 @@ def perform_code_review():
                     payload = {"body": "{}".format(comment)}
                     response = requests.post(url, headers=headers, json=payload)
                     response.raise_for_status()
+d = '''{            "eventName": "CICDUsageEvent",
+                    "eventType": "CICD",
+                    "eventNumber": 4793,
+                    "eventTime": 1696896310332,
+                    "entityGroup": 2,
+                    "entityType": 2,
+                    "entityId": "8122943658466030_259557806380750_3341164-114",
+                    "user": "null",
+                    "queue": "null",
+                    "clusterName": "job-259557806380750-run-3341164",
+                    "clusterUid": "1013-000816-6ap9chzt",
+                    "staticRank": 20,
+                    "dynamicRank": 20.0,
+                    "title": "",
+                    "detail": "",
+                    "actions": ""
+}'''
+
+def index_for_timestamp(prefix, ts):
+    if type(ts) == str:
+        ts = datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%fZ")
+    year = ts.strftime("%Y")
+    month = ts.strftime("%m")
+    day = ts.strftime("%d")
+    d = datetime(int(year), int(month), int(day)).toordinal()
+    sunday = str(datetime.fromordinal(d - (d % 7)))[8:10]
+    saturday = str(datetime.fromordinal(d - (d % 7) + 6))[8:10]
+    return prefix + year + month + sunday + "_" + saturday
+                                
+def send_update_to_unravel(notification_sent,user_ids,jira_link,pr_url,pr_number,repo_name,es_document_list):
+    detail_dict = {}
+    detail_dict['agent'] = "GitHub"
+    detail_dict['repository'] = repo_name
+    detail_dict['prNumber'] = pr_number
+    detail_dict['reviewers'] = user_ids
+    detail_dict['prLink'] = pr_url
+    detail_dict['fileDiff'] = perform_code_review(get_file_name_flag=True)
+    detail_dict['imsLink'] = jira_link
+    detail_dict['notificationSent'] = notification_sent
+    for documents in es_document_list:
+        documents['details'] = detail_dict
+        id = documents['job']
+        documents = json.dumps(documents)
+        index = index_for_timestamp('ev-', documents['eventTime'])
+        body = f'{index} event {id} {5} {documents}'
+        try:
+            r = http.request('PUT', f'{lr_url}/logs/hl/hl/{id}/_bulkr', body=body, headers=headers)
+            if r.status // 100 != 2:
+                print(f'LR request failed: status={r.status} body={body} resp={r.data.decode()}')
+            else:
+                print("eno")
+        except Exception as err:
+            print(f'LR request failed: body={body} error={err}')
+
+    
+
+def create_es_document(gsp, cluster_name, cluster_uid, job):
+    document = {}
+    document['eventName'] = "CICDUsageEvent"
+    document['eventType'] = "CICD"
+    document['eventNumber'] = pr_number
+    document['eventTime'] = int(time.time() * 1000)
+    document['entityGroup'] = 2
+    document['entityType'] = 2
+    document['entityId'] = gsp
+    document['user'] = "null"
+    document['queue'] = "null"
+    document['clusterName'] = cluster_name
+    document['clusterUid'] = cluster_uid
+    document['staticRank'] = 2
+    document['dynamicRank'] = 2
+    document['title'] = "CICD Documnt"
+    document['detail'] = detail_dict
+    document['actions'] = "null"
+    document['job'] = job
+    return document
+    
 
 # %%
 def main():
     # description_json = json.loads(pr_json['description'])
     # job_run_list = get_job_runs_from_description(pr_id, description_json)
     raw_description = get_pr_description()
+    es_document_list = []
     if not raw_description:
         print("Nothing to do without description, skipping!!")
         sys.exit(0)
@@ -598,6 +681,9 @@ def main():
                 run["jir_app_summary"] = jira_summary
                 # add to the list
                 job_run_result_list.append(run)
+                
+                es_document_list.append(create_es_document(gsp,appId,clusterUId, run["job_id"]))
+                
         else:
             print("job_run not found: " + gsp)
 
@@ -633,7 +719,13 @@ def main():
         # Create the message text with user mentions
         message_with_mentions = message + " " + " ".join(formatted_user_ids)
 
-        send_markdown_to_slack(channel, message_with_mentions)
+        try:
+            send_markdown_to_slack(channel, message_with_mentions)
+            notification_sent = True
+        except:
+            notification_sent = False
+
+        send_update_to_unravel(notification_sent,user_ids,jira_link,pr_url,pr_number,repo_name,es_document_list)
 
     else:
         print("Nothing to do without Unravel integration")
