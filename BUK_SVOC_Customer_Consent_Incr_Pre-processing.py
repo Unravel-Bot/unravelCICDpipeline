ba781c3c628f4e1844929bf3bc1e9b95a519d4ec
Unravel code Reviewer <108653287+Unravel-Assistant@users.noreply.github.com>
2024-08-19 23:44:52 +0530

    Update BUK_SVOC_Customer_Consent_Incr_Pre-processing.py
    https://adb-8122943658466030.10.azuredatabricks.net/?o=1953101234786001#job/608329572714989/run/200279578344416

--- BUK_SVOC_Customer_Consent_Incr_Pre-processing.py
+++ BUK_SVOC_Customer_Consent_Incr_Pre-processing.py
@@ -12,7 +12,7 @@
 # MAGIC create widget text SourcePath default "";
 # MAGIC create widget text DestinationPath default "";
 # MAGIC create widget text RunDate default "";
-# MAGIC create widget text RunTime default "";
+# MAGIC create widget text RunTime default ""; 
 
 # COMMAND ----------
 
