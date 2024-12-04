202ae840d705698828609a4d28a0673234b45745
pkalmegh-unravel <50853191+pkalmegh-unravel@users.noreply.github.com>
2024-12-04 10:53:56 +0530

    Update SAPCRM_Advantage_Card_membership_activity_TXT_To_Parquet.py

--- SAPCRM_Advantage_Card_membership_activity_TXT_To_Parquet.py
+++ SAPCRM_Advantage_Card_membership_activity_TXT_To_Parquet.py
@@ -89,6 +89,7 @@ for file in filelist1.split(","):
     df2=spark.createDataFrame(df1,schema=dfSchema1)
     #display(df2)
     df2.write.mode('append').format("parquet").save(tgtmountPoint+DestinationPath+"/"+RunDate+RunTime+"/")
+    deet_df=df.collect()[0][0]
   
 
 # COMMAND ----------
